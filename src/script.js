7a38e60b67ca06490666e9ab26a17c99fe7926ed
Winterh <kotfimn@mail.ru>
2021-10-04 16:29:12 +0700

    pricing, carousel

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
 import './style.scss';
+
+$(".testimonials-carousel").owlCarousel({
+	margin: 30,
+	loop:true,
+	autoplay:true,
+	autoplayTimeout: 2000,
+	autoplayHoverPause:true,
+	mouseDrag:true,
+	touchDrag:true,
+	dotsEach: 1,
+	responsive: {
+		0:{
+			items:1
+		},
+		600:{
+			items:1
+		},
+		1400:{
+			items:2
+		},
+	 2560:{
+		 items:2
+	},
+}
+});
